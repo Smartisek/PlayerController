0a616a75964ed84254b30e6a614fa347f385629c
Smartisek <domalipdominik@gmail.com>
2023-10-19 10:22:52 +0100

    web gl

--- Assets/Scripts/Menu/MainMenuManager.cs
+++ Assets/Scripts/Menu/MainMenuManager.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] private String gameSceneName = "GameScene";
+    [SerializeField] private String optionsSceneName = "OptionsScene";
+
     // Start is called before the first frame update
     void Start()
     {
